b33fc860c435529edd1c8f387f62645fbb76885a
Saryn Mooney <smooney15@my.whitworth.edu>
2021-04-23 10:06:37 -0600

    add joins table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, presence: true 
   validates :summary, presence: true 
   validates :year, presence: true 
+  has_many :actor_movies
+  has_many :actors, through: :actor_movies 
 end
